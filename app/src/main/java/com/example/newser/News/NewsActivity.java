e5c1c307076f367ccc1390aabf824c6ff91cede0
Mojtaba <fazeli.mojtaba@gmail.com>
2019-11-23 00:47:51 -0500

    V.2

--- app/src/main/java/com/example/newser/News/NewsActivity.java
+++ app/src/main/java/com/example/newser/News/NewsActivity.java
@@ -1,17 +1,24 @@
 package com.example.newser.News;
 
+
 import android.os.Bundle;
+
 import android.util.Log;
+
 import android.view.Menu;
 import android.view.MenuItem;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
+import androidx.viewpager.widget.ViewPager;
+
 import com.example.newser.R;
 import com.example.newser.Utils.BottomNavigationViewHelper;
 import com.example.newser.Utils.SectionPagerAdapter;
-import com.example.newser.databinding.LayoutBottomNavigationViewBinding;
-import com.example.newser.databinding.LayoutCenterViewpagerBinding;
-import com.example.newser.databinding.LayoutTopTabsBinding;
+import com.example.newser.databinding.ActivityNewsBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.tabs.TabLayout;
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
+
 
 import java.util.Objects;
 
@@ -19,30 +26,31 @@ import java.util.Objects;
 public class NewsActivity extends AppCompatActivity {
     private static final String TAG = "NewsActivity";
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate ( savedInstanceState );
-        setContentView ( R.layout.activity_news);
+        ActivityNewsBinding activityNewsBinding = DataBindingUtil.setContentView(this, R.layout.activity_news);
 
         setupBottomNavigationView ();
         setupViewPager();
     }
 
     private void setupViewPager (){
-        LayoutCenterViewpagerBinding layoutCenterViewpagerBinding = DataBindingUtil.setContentView(this,R.layout.layout_center_viewpager);
         SectionPagerAdapter adapter = new SectionPagerAdapter ( getSupportFragmentManager () );
         adapter.addFragment ( new AllFragment () );
         adapter.addFragment ( new WorldFragment () );
         adapter.addFragment ( new CountryFragment () );
         adapter.addFragment ( new CityFragment () );
-        layoutCenterViewpagerBinding.container.setAdapter ( adapter );
 
-        LayoutTopTabsBinding layoutTopTabsBinding = DataBindingUtil.setContentView(this, R.layout.layout_top_tabs);
-        layoutTopTabsBinding.tabs.setupWithViewPager ( layoutCenterViewpagerBinding.container );
-        Objects.requireNonNull(layoutTopTabsBinding.tabs.getTabAt(0)).setIcon(R.drawable.ic_all);
-        Objects.requireNonNull(layoutTopTabsBinding.tabs.getTabAt(1)).setIcon (R.drawable.ic_world);
-        Objects.requireNonNull(layoutTopTabsBinding.tabs.getTabAt(2)).setIcon (R.drawable.ic_country);
-        Objects.requireNonNull(layoutTopTabsBinding.tabs.getTabAt(3)).setIcon (R.drawable.ic_city);
+        ViewPager viewPager = findViewById(R.id.container);
+        viewPager.setAdapter ( adapter );
+        TabLayout tabs =  findViewById(R.id.tabs);
+        tabs.setupWithViewPager(viewPager);
+        Objects.requireNonNull(tabs.getTabAt(0)).setIcon(R.drawable.ic_all);
+        Objects.requireNonNull(tabs.getTabAt(1)).setIcon (R.drawable.ic_world);
+        Objects.requireNonNull(tabs.getTabAt(2)).setIcon (R.drawable.ic_country);
+        Objects.requireNonNull(tabs.getTabAt(3)).setIcon (R.drawable.ic_city);
     }
 
     /*
@@ -50,10 +58,10 @@ public class NewsActivity extends AppCompatActivity {
      */
     private void setupBottomNavigationView() {
         Log.d ( TAG, "setupBottomNavigationView: setting up BottomNavigationView" );
-        LayoutBottomNavigationViewBinding layoutBottomNavViewBinding = DataBindingUtil.setContentView(this, R.layout.layout_bottom_navigation_view);
-        BottomNavigationViewHelper.setupBottomNavigationView (  layoutBottomNavViewBinding.bottomNavViewBar );
-        BottomNavigationViewHelper.enableNavigation ( NewsActivity.this,  layoutBottomNavViewBinding.bottomNavViewBar );
-        Menu menu =  layoutBottomNavViewBinding.bottomNavViewBar.getMenu ();
+        BottomNavigationViewEx bottomNavigationView = findViewById(R.id.bottomNavViewBar);
+        BottomNavigationViewHelper.setupBottomNavigationView ( bottomNavigationView );
+        BottomNavigationViewHelper.enableNavigation ( NewsActivity.this,  bottomNavigationView );
+        Menu menu =  bottomNavigationView.getMenu ();
         MenuItem item = menu.getItem(1);
         item.setChecked ( true );
     }
