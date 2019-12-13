867c3c4e9bccb3a89c5ee29a8fe817eded597cf4
Mojtaba <fazeli.mojtaba@gmail.com>
2019-12-13 10:26:17 -0500

    v 8. Adding Permissions

--- app/src/main/java/com/example/newser/Couch/CouchActivity.java
+++ app/src/main/java/com/example/newser/Couch/CouchActivity.java
@@ -1,5 +1,7 @@
 package com.example.newser.Couch;
 
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -7,11 +9,13 @@ import android.view.MenuItem;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.databinding.DataBindingUtil;
 
 import com.example.newser.News.NewsActivity;
 import com.example.newser.R;
 import com.example.newser.Utils.BottomNavigationViewHelper;
+import com.example.newser.Utils.Permissions;
 import com.example.newser.databinding.ActivityNewsNewBinding;
 import com.example.newser.databinding.LayoutBottomNavigationViewBinding;
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
@@ -19,15 +23,53 @@ import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 
 public class CouchActivity extends AppCompatActivity {
     private static final String TAG = "CouchActivity";
+    private static final int ACTIVITY_NUMBER = 2;
+
+    private Context mContext = CouchActivity.this;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate ( savedInstanceState );
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news_new);
         Log.d(TAG, "onCreate: started");
 
-        setupBottomNavigationView ();
+        if(checkPermissionsArray(Permissions.PERMISSIONS)) {
 
+        }else{
+            verifyPermissionsArray(Permissions.PERMISSIONS);
+        }
+
+        setupBottomNavigationView();
+
+    }
+
+    private void verifyPermissionsArray(String[] permissions) {
+        ActivityCompat.requestPermissions(
+                CouchActivity.this,
+                permissions,
+                1
+        );
+    }
+
+    private boolean checkPermissionsArray(String[] permissions) {
+        Log.d(TAG, "checkPermissons started");
+
+        for (int i = 0; i < permissions.length; i++) {
+            String check = permissions[i];
+            if (!checkPermissions(check)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean checkPermissions(String permission) {
+        int permissionRequest = ActivityCompat.checkSelfPermission(CouchActivity.this, permission);
+        if (permissionRequest != PackageManager.PERMISSION_GRANTED) {
+            return false;
+        } else {
+            return true;
+        }
     }
 
     /*
--- app/src/main/java/com/example/newser/Login/LoginActivity.java
+++ app/src/main/java/com/example/newser/Login/LoginActivity.java
@@ -18,12 +18,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.newser.News.NewsActivity;
 import com.example.newser.R;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.FirebaseDatabase;
 
 public class LoginActivity extends AppCompatActivity {
     private static final String TAG = "LoginActivity";
@@ -42,8 +38,8 @@ public class LoginActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mAuth = FirebaseAuth.getInstance();
-//        ActivityLoginNewBinding activityLoginBinding = DataBindingUtil.setContentView(this, R.layout.activity_login_new);
-        setContentView(R.layout.activity_login_new);
+//        ActivityLoginNewBinding activityLoginBinding = DataBindingUtil.setContentView(this, R.layout.activity_login);
+        setContentView(R.layout.activity_login);
         mProgressBar = findViewById(R.id.progressBar);
         mProgressBar.setVisibility(View.GONE);
 
